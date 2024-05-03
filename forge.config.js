a49dc7024ccd3bfae5d7d9e2debc34d763d13b8a
GideonWhite1029 <gideonwhite1029@gmail.com>
2024-05-03 23:33:16 +0400

    first preparation for version 1.5

---
+++ forge.config.js
@@ -0,0 +1,46 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+
+module.exports = {
+  packagerConfig: {
+    author: "ymata, GideonWhite1029",
+    description: "Приложение для просмотров фильмов и сериалов",
+    arch: ["x64", "ia32"],
+    platform: ["darwin", "linux", "win32"],
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin', 'linux'],
+    },
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        maintainer: 'GideonWhite1029',
+        homepage: 'https://github.com/GideonWhite1029/lampa-desktop'
+      },
+    }
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+  buildIdentifier: "com.lampa.mx"
+};
---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      experimentalFeatures: true,
+      nodeIntegration: true
+    },
+  });
+
+  mainWindow.setMenu(null)
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
